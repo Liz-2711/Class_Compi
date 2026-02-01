48fc3c7a4ad6a7c89c56a5accc42c8fd964947f6
Liz-2711 <lizjrz11@unitec.edu>
2026-02-01 08:55:14 -0600

    SEMANA2

---
+++ tokens.hpp
@@ -0,0 +1,14 @@
+#ifndef TOKENS_HPP
+#define TOKENS_HPP
+enum class Tokens{
+     END_OF_FILE ,
+     // KEYWORD_IF,
+     // OP_MUL,
+     // OP_ASSIGN,
+     ASCII_IDENTIFIER ,
+CONST_NUMBER ,
+CONST_STRING 
+};
+
+
+#endif
